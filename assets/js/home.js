e035fe551157d984e6d69b7c50a535bd21755788
Samrat Dutta <dutta.sam.d@gmail.com>
2019-07-21 00:31:55 +0200

    Sandakphu page setup

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,52 +7,60 @@ $(document).ready(
     }
 );
 
-$(window).scroll(
-    function () {
-        var wScroll = $(document).scrollTop();
-        var winHeight = $(window).height();
 
-        var buoypos = document.getElementById("el").getBoundingClientRect();
-        
 
-        $(".caption").css(
-            "transform", "translate(0px, " + wScroll / 3 + "%)"
+var throttledparallax = _.throttle(() => { 
+    parallax();
+}, 10);
+
+function parallax() {
+
+    console.log("In parallax");
+    var wScroll = $(document).scrollTop();
+    var winHeight = $(window).height();
+
+    var buoypos = document.getElementById("el").getBoundingClientRect();
+    
+
+    $(".caption").css(
+        "transform", "translate(0px, " + wScroll / 3 + "%)"
+    );
+    $(".parallax").css(
+        "transform",
+        "translate(0px, " + -wScroll / 20 + "%)"
+    );
+
+    var scrollpercent = (buoypos.top / winHeight) * 100;
+
+    if (scrollpercent < 9) {
+        $(".navbar-brand").css(
+            "color",
+            "darkslategray"
         );
-        $(".parallax").css(
-            "transform",
-            "translate(0px, " + -wScroll / 20 + "%)"
+        $(".nav-link").css(
+            "color",
+            "darkslategray"
         );
-
-        var scrollpercent = (buoypos.top / winHeight) * 100;
-        console.log(scrollpercent);
-        if (scrollpercent < 9) {
-            $(".navbar-brand").css(
-                "color",
-                "darkslategray"
-            );
-            $(".nav-link").css(
-                "color",
-                "darkslategray"
-            );
-            $(".fas.fa-bars").css(
-                "color",
-                "darkslategray"
-            );
-            $("#nav").addClass("shrink");
-        } else {
-            $(".navbar-brand").css(
-                "color",
-                "white"
-            );
-            $(".nav-link").css(
-                "color",
-                "white"
-            );
-            $(".fas.fa-bars").css(
-                "color",
-                "white"
-            );
-            $("#nav").removeClass("shrink");
-        }
+        $(".fas.fa-bars").css(
+            "color",
+            "darkslategray"
+        );
+        $("#nav").addClass("shrink");
+    } else {
+        $(".navbar-brand").css(
+            "color",
+            "white"
+        );
+        $(".nav-link").css(
+            "color",
+            "white"
+        );
+        $(".fas.fa-bars").css(
+            "color",
+            "white"
+        );
+        $("#nav").removeClass("shrink");
     }
-);
+}
+
+window.addEventListener('scroll', throttledparallax);
---
+++ assets/js/travelblog.js
@@ -0,0 +1,82 @@
+var throttledparallax = _.throttle(() => { 
+    parallax();
+}, 10);
+
+var isnavsolid = false;
+var iscaptionblack = false;
+
+
+function parallax() {
+
+    console.log("In parallax");
+    var wScroll = $(document).scrollTop();
+    // var winHeight = $(window).height();
+
+    var scrollTop = $(window).scrollTop();
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var scrollPercent = (scrollTop) / winHeight;
+
+    console.log(scrollPercent);
+
+    $(".caption").css(
+        "transform",
+        "translateY(" + -scrollPercent * 50 + "vh)"
+    );
+
+    if (scrollPercent > 0.25) {
+        if(!iscaptionblack) {
+            $(".caption").css(
+                "color",
+                "black"
+            );
+            iscaptionblack = true;
+        }
+
+        if(scrollPercent > 0.50 && !isnavsolid) {
+            $(".navbar-brand").css(
+                "color",
+                "darkslategray"
+            );
+            $(".nav-link").css(
+                "color",
+                "darkslategray"
+            );
+            $(".fas.fa-bars").css(
+                "color",
+                "darkslategray"
+            );
+            $("#nav").addClass("shrink");
+            isnavsolid = true;
+        }
+        
+    } 
+    if(scrollPercent < 0.50) {
+        if(scrollPercent < 0.25 && iscaptionblack) {
+            $(".caption").css(
+                "color",
+                "white"
+            );
+            iscaptionblack = false;
+        }
+
+        if(isnavsolid) {
+            $(".navbar-brand").css(
+                "color",
+                "white"
+            );
+            $(".nav-link").css(
+                "color",
+                "white"
+            );
+            $(".fas.fa-bars").css(
+                "color",
+                "white"
+            );
+            $("#nav").removeClass("shrink");
+            isnavsolid = false;
+        }
+    }
+}
+
+window.addEventListener('scroll', throttledparallax);
