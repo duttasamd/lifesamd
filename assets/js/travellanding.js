3da3a12f13aa99359285323c3fb82f77271a3608
Samrat Dutta <dutta.sam.d@gmail.com>
2019-07-22 11:19:36 +0200

    Basic structure Gokarna and NYC Pages

--- assets/js/travellanding.js
+++ assets/js/travellanding.js
@@ -1,3 +1,11 @@
 $("#cardsandk").click(function() {
 	window.location.href = "sandakphu.html";
 });
+
+$("#cardgokarna").click(function() {
+	window.location.href = "gokarna.html";
+});
+
+$("#cardnyc").click(function() {
+	window.location.href = "nyc.html";
+});
