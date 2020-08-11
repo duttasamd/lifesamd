d2e218cffe0a2dd837ca43309b75f48176969d85
Samrat Dutta <dutta.sam.d@gmail.com>
2020-08-11 04:56:28 +0200

    Added tech landing page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,7 @@
 $("#travelblog").click(function() {
 	window.location.href = "travelblog/travellanding.html";
 });
+
+$("#techblog").click(function() {
+	window.location.href = "techblog/techlanding.html";
+});
